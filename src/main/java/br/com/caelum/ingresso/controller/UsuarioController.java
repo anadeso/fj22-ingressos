380f7561a40933c8614054a0c0f07beba85dfea3
Ana Sousa <anadeso90@gmail.com>
2020-04-28 22:37:56 -0300

    validando token

--- src/main/java/br/com/caelum/ingresso/controller/UsuarioController.java
+++ src/main/java/br/com/caelum/ingresso/controller/UsuarioController.java
@@ -1,15 +1,20 @@
 package br.com.caelum.ingresso.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.helper.TokenHelper;
 import br.com.caelum.ingresso.mail.EmailNovoUsuario;
 import br.com.caelum.ingresso.mail.Mailer;
 import br.com.caelum.ingresso.model.Token;
+import br.com.caelum.ingresso.model.form.ConfirmacaoLoginForm;
 
 @Controller
 public class UsuarioController {
@@ -32,6 +37,26 @@ public class UsuarioController {
 		
 		return view;
 	}
-
+	
+	@GetMapping("/usuario/validate")
+	public ModelAndView validaLink(@RequestParam("uuid") String uuid){
+		
+		Optional<Token> optionalToken = tokenHelper.getTokenFrom(uuid);
+		
+		if (!optionalToken.isPresent()){
+			ModelAndView view = new ModelAndView("redirect:/login");
+			
+			view.addObject("msg", "O token do link utilizado não foi encontrado!");
+			
+			return view;
+		}
+		
+		Token token = optionalToken.get();
+		ConfirmacaoLoginForm confirmacaoLoginForm = new ConfirmacaoLoginForm(token);
+		
+		ModelAndView view = new ModelAndView("usuario/confirmacao");
+		view.addObject("confirmacaoLoginForm", confirmacaoLoginForm);
+		return view;
+	}
 	
 }
