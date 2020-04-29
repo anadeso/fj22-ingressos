1fd134bf0e644a7d36ec83423596fd821401057e
Ana Sousa <anadeso90@gmail.com>
2020-04-28 22:03:55 -0300

    implementando envio de emaill

--- src/main/java/br/com/caelum/ingresso/controller/UsuarioController.java
+++ src/main/java/br/com/caelum/ingresso/controller/UsuarioController.java
@@ -1,16 +1,37 @@
 package br.com.caelum.ingresso.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.caelum.ingresso.helper.TokenHelper;
+import br.com.caelum.ingresso.mail.EmailNovoUsuario;
+import br.com.caelum.ingresso.mail.Mailer;
+import br.com.caelum.ingresso.model.Token;
+
 @Controller
 public class UsuarioController {
+	
+	@Autowired
+	private Mailer mailer;
+	@Autowired
+	private TokenHelper tokenHelper;
+	
 
 	@GetMapping("/usuario/request")
-	public ModelAndView formSolicitacaoDeAcesso() {
-		return new ModelAndView("usuario/form-email");
-
+	@Transactional
+	public ModelAndView solicitacaoDeAcesso(String email){
+		
+		ModelAndView view = new ModelAndView("usuario/adicionado");
+		
+		Token token = tokenHelper.generateFrom(email);
+		
+		mailer.send( new EmailNovoUsuario(token) );
+		
+		return view;
 	}
 
+	
 }
